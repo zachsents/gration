b200f41145886596a9e29b76324025044f0ddff7
Zach Sents <zachsents@gmail.com>
2023-11-01 15:25:52 -0600

    Added pricing info to new landing page

--- web/components/landing/PricingSection.jsx
+++ web/components/landing/PricingSection.jsx
@@ -1,13 +1,19 @@
-import { Button, Center, Divider, Group, Stack, Text, ThemeIcon } from "@mantine/core"
+import { Button, Center, Divider, Group, Stack, Switch, Text, ThemeIcon } from "@mantine/core"
 import { useBillingLinks } from "@web/modules/stripe"
 import classNames from "classnames"
 import Link from "next/link"
+import { useState } from "react"
 import { TbCheck } from "react-icons/tb"
 import { useUser } from "reactfire"
+import { FREE_ACCOUNT_LIMIT } from "shared/stripe"
 import Section, { SectionHeader } from "./Section"
 
 
 export default function PricingSection() {
+
+    const [annual, setAnnual] = useState(false)
+
+
 
     return (
         <Section id="pricing">
@@ -17,20 +23,71 @@ export default function PricingSection() {
                     Integrations affordable for everyone
                 </h4>
                 <Text className="text-xl">
-                    Building valuable SaaS tools means integrating with your users' data, but adding third-party integrations is complicated.
+                    WoahAuth is free for the first {FREE_ACCOUNT_LIMIT} connected accounts, then we offer affordable pricing plans for growing SaaS companies.
                 </Text>
             </SectionHeader>
 
             <div className="grid grid-cols-3 gap-10">
-                <PriceCard productName="starter" />
-                <PriceCard productName="starter" recommended />
-                <PriceCard productName="starter" />
+                <PriceCard
+                    productName="starter"
+                    annual={annual}
+                    annualPrice={199} monthlyPrice={19}
+                    description="For small projects and personal use"
+                    features={[
+                        "Unlimited integration clients",
+                        "Up to 1,000 connected accounts",
+                        "API key integrations",
+                    ]}
+                />
+                <PriceCard
+                    productName="business"
+                    annual={annual}
+                    annualPrice={949} monthlyPrice={89}
+                    description="For growing SaaS companies"
+                    features={[
+                        "Unlimited integration clients",
+                        "Up to 50,000 connected accounts",
+                        "API key integrations",
+                        "Integration Portal for your users",
+                    ]}
+                    recommended
+                />
+                <PriceCard
+                    productName="enterprise"
+                    annual={annual}
+                    annualPrice={2199} monthlyPrice={199}
+                    description="For established SaaS companies with large userbases"
+                    features={[
+                        "Unlimited integration clients",
+                        "Up to 1,000,000 connected accounts",
+                        "Integration Portal for your users",
+                        "API key integrations",
+                        "Direct support line",
+                    ]}
+                    cta={<Button
+                        variant="white" color="pg"
+                        component="a" href="mailto:info@woahauth.com?subject=Enterprise+Inquiry"
+                    >
+                        Contact Us
+                    </Button>}
+                />
             </div>
+
+            <Center className="mt-12 flex-col gap-2">
+                <Text className="text-md">
+                    Billed <span className={!annual && "font-bold"}>monthly</span> / <span className={annual && "font-bold"}>annually</span>
+                </Text>
+                <Switch
+                    checked={annual} onChange={event => setAnnual(event.currentTarget.checked)}
+                    classNames={{ track: "cursor-pointer" }}
+                    size="lg"
+                />
+            </Center>
         </Section>
     )
 }
 
-function PriceCard({ productName, annual = false, recommended = false }) {
+function PriceCard({ productName, description, annual = false, annualPrice, monthlyPrice, features = [], recommended = false, cta }) {
 
     const { data: user } = useUser()
 
@@ -45,33 +102,33 @@ function PriceCard({ productName, annual = false, recommended = false }) {
                 <Text className="absolute bottom-full left-0 w-full text-center pb-2 uppercase text-primary-800 text-sm font-bold">Recommended</Text>}
 
             <Text className="text-xl font-bold my-2">
-                Prototype
+                {productName[0].toUpperCase() + productName.slice(1)}
             </Text>
             <Text className="text-md">
-                For small projects and personal use
+                {description}
             </Text>
 
             <Divider className="my-4" color={recommended ? "pg" : "pg.1"} />
 
             <Group className="items-end gap-1">
                 <Text className="text-4xl text-pg font-bold">
-                    $49
+                    ${annual ? annualPrice : monthlyPrice}
                 </Text>
                 <Text className="text-sm">
-                    /month
+                    /{annual ? "year" : "month"}
                 </Text>
             </Group>
 
             <Divider className="my-4" color={recommended ? "pg" : "pg.1"} />
 
             <Stack className="gap-xs">
-                <FeatureLine>up to 100,000 connected accounts</FeatureLine>
-                <FeatureLine>Integration Portal for your users</FeatureLine>
-                <FeatureLine>API key integrations</FeatureLine>
+                {features.map((feature, i) => (
+                    <FeatureLine key={i}>{feature}</FeatureLine>
+                ))}
             </Stack>
 
             <Center className="mt-6">
-                {user ?
+                {cta || (user ?
                     <Button
                         variant="white" color={recommended ? "primary" : "pg"}
                         onClick={createSession} loading={sessionMutation.isLoading}
@@ -83,7 +140,7 @@ function PriceCard({ productName, annual = false, recommended = false }) {
                         component={Link} href="/login?register&redirect=/dashboard/billing"
                     >
                         Sign Up
-                    </Button>}
+                    </Button>)}
             </Center>
         </div>
     )
